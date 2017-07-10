5b6abd2f53f3a085229083bd578696cf56be6a30
8096819402 <gani120077@gmail.com>
2017-07-10 15:20:37 +0530

    Add files via upload

--- open.py
+++ open.py
@@ -4,13 +4,23 @@ import threading
 import Queue
 
 common_ports={
-
+    
+          "7":"Echo",
           "21":"ftp",
           "22":"ssh",
+          "23":"Telnet",
+          "25":"smtp",
+          "53":"DNS",
+          "79":"Finger",
           "80":"http",
+          "110":"POP3",
+          "143":"IMAP4",
+          "201":"AppleTalk",
           "443":"https",
-          "135":"rk"
-    }
+          "520":"RIP",
+          "902":"VMware Sever"
+         
+           }
 
 def is_port_open(host,port):
     try:
@@ -20,22 +30,21 @@ def is_port_open(host,port):
     except socket.error:
         return False
     return True
-def scanner(host):
+def scan(host):
     while True:
         port =port_queue.get()
         if is_port_open(host,port):
             if str(port) in common_ports:
-                print("{} is Open".format(str(port),common_ports[str(port)]))
+                print("{}({}) is Open".format(str(port),common_ports[str(port)]))
             else:
                 print("{} is Open".format(port))
         port_queue.task_done()
 port_queue=Queue.Queue()
 for _ in range(20):
-    t=threading.Thread(target=scanner,kwargs={"host":sys.argv[1]})
+    t=threading.Thread(target=scan,kwargs={"host":sys.argv[1]})
     t.daemon=True
     t.start()
 for port in range(1024):
     port_queue.put(port)
 
 port_queue.join()
-
